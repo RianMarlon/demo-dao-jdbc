5ab49dafab8eccb801bab490ccd4630abb2e9980
Rian Marlon <rianmarlon20@yahoo.com>
2019-04-07 15:45:43 -0300

    Classe de entidade Seller

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -1,6 +1,9 @@
 package aplicacao;
 
+import java.util.Date;
+
 import modelo.entidade.Department;
+import modelo.entidade.Seller;
 
 public class Principal {
 
@@ -8,7 +11,10 @@ public class Principal {
 		
 		Department obj = new Department (1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller (21, "Bob", "bob@gemail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
-
 }
