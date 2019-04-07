6418ee68c826d03ba815eb12d6449a1091f50048
Rian Marlon <rianmarlon20@yahoo.com>
2019-04-07 15:31:56 -0300

    Classe de entidade Department

---
+++ src/aplicacao/Principal.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import modelo.entidade.Department;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department (1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
